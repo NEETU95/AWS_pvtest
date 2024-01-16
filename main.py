0883487638e812b15f0358d47b024c2424940e08
NEETU95 <neetu0708@gmail.com>
2024-01-16 10:14:43 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ import re
 from for_country import get_country
 from receipt_file import get_receipt
 import json
+from events_tab import reaction_event
 
 from metapub import PubMedFetcher
 
@@ -84,8 +85,12 @@ def pdf_extraction(event=None, context=None):
         nlp = spacy.load("en_core_web_sm")
         nlp_1 = spacy.load("/var/task/en_ner_bc5cdr_md")
         # Loop through all pages and extract text
+        first_page = source_file_reader.pages[0]
+        first_page_text = first_page.extract_text()
+        # print(first_page_text)
         for page_num in range(source_file_num_pages):
             page = source_file_reader.pages[page_num]
+
             text = page.extract_text()
             if "References" in text or "Bibliography" in text:
                 references_found = True
@@ -99,6 +104,8 @@ def pdf_extraction(event=None, context=None):
         title_of_page = ""
         print("title_of_page", meta.title)
         doi = ""
+        doi_found = False
+        doi_raw = ""
         json_response_from_api = {}
         try:
             if not title_of_page or len(title_of_page.split()) < 2:
@@ -107,12 +114,63 @@ def pdf_extraction(event=None, context=None):
                 # Iterate through the lines
                 for i, line in enumerate(all_text.split("\n")):
                     if "DOI:" in line or "doi:" in line:
-                        text_up_to_doi_for_author = line
-                        break  # Stop when the line containing "DOI:" is found
-                affiliations = text_up_to_doi_for_author.split("\n")
-                index_doi = text_up_to_doi_for_author.find('DOI:')
-                doi_raw = text_up_to_doi_for_author[index_doi + len('doi:'):].strip()
-                doi = re.sub(r'[^\x00-\x7F]+', '', doi_raw)
+                        if i + 1 < len(all_text.split('\n')):
+                            text_up_to_doi_for_author = line
+                            # print("text upto doi author", text_up_to_doi_for_author)
+                            affiliations = text_up_to_doi_for_author.split("\n")
+                            lowercase_text_doi_for_author = text_up_to_doi_for_author.lower()
+                            index_doi = lowercase_text_doi_for_author.find('doi:')
+                            doi_raw = text_up_to_doi_for_author[index_doi + len('doi:'):].strip()
+                            print("doi_raw is", doi_raw)
+                            doi = re.sub(r'[^\x00-\x7F]+', '', doi_raw)
+                            doi_found = True
+                            print("doi", doi)
+                            break
+                    else:
+                        https_index = first_page_text.find("http")
+
+                        # If "https" is found, proceed to extract DOI
+                        if https_index != -1:
+                            # Extract the substring starting from "https"
+                            # Extract the substring starting from "https"
+                            substring = first_page_text[https_index:]
+
+                            urls = re.findall(
+                                r'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+',
+                                substring)
+
+                            # Check if any URLs were found
+                            if urls:
+                                print("yes")
+                                # Extract the text before the URL
+                                url_parts = urls[0].split('/')
+                                doi_number = '/'.join(url_parts[3:])
+                                doi = doi_number
+                                print(doi_number)
+
+                            doi_found = True
+                            print("doi", doi)
+                if not doi:
+                    for i, line in enumerate(all_text.split("\n")):
+                        if "DOI:" in line or "doi:" in line:
+                            print("yupp")
+                            if i + 1 < len(all_text.split('\n')):
+                                print("superr")
+                                next_line = all_text.split('\n')[i + 1]
+                                print(f"Next line {i + 2}: {next_line}")
+                                text_up_to_doi_for_author = line + ' ' + next_line
+                                # print("text upto doi author", text_up_to_doi_for_author)
+                                affiliations = text_up_to_doi_for_author.split("\n")
+                                lowercase_text_doi_for_author = text_up_to_doi_for_author.lower()
+                                index_doi = lowercase_text_doi_for_author.find('doi:')
+                                doi_raw = text_up_to_doi_for_author[index_doi + len('doi:'):].strip()
+                                print("doi_raw is", doi_raw)
+                                doi = re.sub(r'[^\x00-\x7F]+', '', doi_raw)
+                                doi_found = True
+                                print("doi", doi)
+                                break
+                        # Stop when the line containing "DOI:" is found
+
                 if doi:
                     print("yes into pubmed")
                     fetch = PubMedFetcher()
@@ -122,7 +180,7 @@ def pdf_extraction(event=None, context=None):
                     article = fetch.article_by_pmid(pmid)
                     # article = fetch.article_by_doi(doi)
                     title_of_page_1 = article.title
-                    if title_of_page_1 and len(title_of_page_1.split())>2:
+                    if title_of_page_1 and len(title_of_page_1.split()) > 2:
                         print("title length is >2")
                         title_of_page = title_of_page_1
         except Exception as e:
@@ -136,6 +194,7 @@ def pdf_extraction(event=None, context=None):
                     "first_file_name": weekly_reader_1,
                     "second_file_name": source_document,
                 }
+                print(response)
                 print(json.dumps(response))
                 url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
                 print(
@@ -145,13 +204,14 @@ def pdf_extraction(event=None, context=None):
                 print(response_from_api.text)
                 json_response_from_api = response_from_api.json()
                 # Send the POST request with JSON data
-                print(response)
 
         country_verify = ""
         latest_receipt_date = ""
         try:
             print("checking in 2nd blcok")
             latest_receipt_date = get_receipt(en_core=nlp, weekly_text_1=weekly_text)
+            if not latest_receipt_date:
+                raise Exception("latest receipt date is not found")
 
         except Exception as e:
             print(f"Exception occurred: {str(e)}")
@@ -163,11 +223,12 @@ def pdf_extraction(event=None, context=None):
                     "first_file_name": weekly_reader_1,
                     "second_file_name": source_document
                 }
+                print(response)
                 url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
                 print(
                     "=--------------------------------------------------------------------------------------------------------")
                 # Send the POST request with JSON data
-                print(response)
+
                 response_from_api = requests.post(url, json=response)
                 print("*" * 50)
                 print(response_from_api.text)
@@ -175,6 +236,9 @@ def pdf_extraction(event=None, context=None):
         try:
             print("checking in 3rd block")
             country_verify = get_country(title=title_of_page, weekly_text_1=weekly_text, en_core=nlp)
+            print("country is", country_verify)
+            if not country_verify:
+                raise Exception("country not found")
 
         except Exception as e:
             print(f"Exception occurred: {str(e)}")
@@ -184,11 +248,12 @@ def pdf_extraction(event=None, context=None):
                 "first_file_name": weekly_reader_1,
                 "second_file_name": source_document,
             }
+            print(response)
             url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
             print(
                 "=--------------------------------------------------------------------------------------------------------")
             # Send the POST request with JSON data
-            print(response)
+
             response_from_api = requests.post(url, json=response)
             print("*" * 50)
             print(response_from_api.text)
@@ -203,26 +268,28 @@ def pdf_extraction(event=None, context=None):
                     source_text=all_text,
                     weekly_text_1=weekly_text,
                     en_core=nlp,
-                    meta_data=meta
+                    meta_data=meta,
+                    first_page=first_page_text
                 )
         except Exception as e:
-                print(f"Exception occurred: {str(e)}")
-                response = {
-                    "success": False,
-                    "message": f"Exception occurred from general_reporter: {str(e)}",
-                    "first_file_name": weekly_reader_1,
-                    "second_file_name": source_document
-                }
-                url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
-                print(
-                    "=--------------------------------------------------------------------------------------------------------")
-                # Send the POST request with JSON data
-                print(response)
-                response_from_api = requests.post(url, json=response)
-                print("*" * 50)
-                print("repsonse from api",response_from_api.text)
-                json_response_from_api = response_from_api.json()
-                # print(general_extraction, reporter_extraction)
+            print(f"Exception occurred: {str(e)}")
+            response = {
+                "success": False,
+                "message": f"Exception occurred from general_reporter: {str(e)}",
+                "first_file_name": weekly_reader_1,
+                "second_file_name": source_document
+            }
+            print(response)
+            url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
+            print(
+                "=--------------------------------------------------------------------------------------------------------")
+            # Send the POST request with JSON data
+            print(response)
+            response_from_api = requests.post(url, json=response)
+            print("*" * 50)
+            print("repsonse from api",response_from_api.text)
+            json_response_from_api = response_from_api.json()
+            # print(general_extraction, reporter_extraction)
         try:
             patient_extraction = get_patient_text(source_text=all_text, en_core=nlp, bcd5r=nlp_1)
         except Exception as e:
@@ -233,11 +300,13 @@ def pdf_extraction(event=None, context=None):
                 "first_file_name": weekly_reader_1,
                 "second_file_name": source_document
             }
+            print(response)
             url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
+
             print(
                 "=--------------------------------------------------------------------------------------------------------")
             # Send the POST request with JSON data
-            print(response)
+
             response_from_api = requests.post(url, json=response)
             print("*" * 50)
             print(response_from_api.text)
@@ -252,59 +321,59 @@ def pdf_extraction(event=None, context=None):
                 "first_file_name": weekly_reader_1,
                 "second_file_name": source_document
             }
+            print(response)
             url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
             print(
                 "=--------------------------------------------------------------------------------------------------------")
             # Send the POST request with JSON data
-            print(response)
+
             response_from_api = requests.post(url, json=response)
             print("*" * 50)
             print(response_from_api.text)
             json_response_from_api = response_from_api.json()
 
+        response_for_integration = {
+            "success": True,
+            "message": "Extracted successfully",
+            "first_file_name": weekly_reader_1,
+            "second_file_name": source_document,
+            "general_information": general_extraction,
+            "reporter": reporter_extraction,
+            "patient": patient_extraction,
+            "parent": parent_extraction
+        }
+        print("response of NLP model", json.dumps(response_for_integration))
 
+        url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
+        print(
+            "=--------------------------------------------------------------------------------------------------------")
+        # Send the POST request with JSON data
+        response = requests.post(url, json=response_for_integration)
+        json_response_from_api = response.json()
+        print("*" * 50)
+        # Check the response status code
+        if json_response_from_api['success'] == 'true':
+            # Request was successful
+            print("API request successful.")
+            # print("Status Code:", json_response_from_api['statusCode'])
+            # print("Response Headers:", response.headers)
+            os.chdir('..')
+            shutil.rmtree('random_temp', ignore_errors=True)
+            os.chdir('..')
+            return {'success': json_response_from_api['success'], 'body': json.dumps(
+                {"data": 'API request successful', "error ": {'msg': str("Status Code: 200")}, "status": 5})}
 
-            response_for_integration = {
-                "success": True,
-                "message": "Extracted successfully",
-                "first_file_name": weekly_reader_1,
-                "second_file_name": source_document,
-                "general_information": general_extraction,
-                "reporter": reporter_extraction,
-                "patient": patient_extraction,
-                "parent": parent_extraction
-            }
-
-            url = "https://demo1.topiatech.co.uk/PV/createCaseAI"
+        else:
+            # Request failed
+            os.chdir('..')
+            shutil.rmtree('random_temp', ignore_errors=True)
+            os.chdir('..')
             print(
-                "=--------------------------------------------------------------------------------------------------------")
-            # Send the POST request with JSON data
-            response = requests.post(url, json=response_for_integration)
-            json_response_from_api = response.json()
-            print("*" * 50)
-            # Check the response status code
-            if json_response_from_api['success'] == 'true':
-                # Request was successful
-                print("API request successful.")
-                # print("Status Code:", json_response_from_api['statusCode'])
-                # print("Response Headers:", response.headers)
-                os.chdir('..')
-                shutil.rmtree('random_temp', ignore_errors=True)
-                os.chdir('..')
-                return {'success': json_response_from_api['success'], 'body': json.dumps(
-                    {"data": 'API request successful', "error ": {'msg': str("Status Code: 200")}, "status": 5})}
-
-            else:
-                # Request failed
-                os.chdir('..')
-                shutil.rmtree('random_temp', ignore_errors=True)
-                os.chdir('..')
-                print(
-                    f"API request failed with status code {json_response_from_api['success']}: {json_response_from_api['message']}")
-                # print(response.text)
-                return {'failure': json_response_from_api['success'],
-                        'body': json.dumps({"data": 'API request not success ', "error ": {
-                            'msg': str(f"API request failure with {json_response_from_api['success']}")}})}
+                f"API request failed with status code {json_response_from_api['success']}: {json_response_from_api['message']}")
+            # print(response.text)
+            return {'failure': json_response_from_api['success'],
+                    'body': json.dumps({"data": 'API request not success ', "error ": {
+                        'msg': str(f"API request failure with {json_response_from_api['success']}")}})}
 
     except Exception as e:
 
